e170f70dc10e4ae808dee09667aea163473a8d69
Bill Randall <william.randall@cru.org>
2020-06-30 10:40:12 -0400

    Client can be final

--- src/main/java/org/cru/aemscraper/service/impl/AemScraperServiceImpl.java
+++ src/main/java/org/cru/aemscraper/service/impl/AemScraperServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AemScraperServiceImpl implements AemScraperService {
-    private Client client;
+    private final Client client;
 
     public AemScraperServiceImpl() {
         this(ClientBuilder.newClient());
